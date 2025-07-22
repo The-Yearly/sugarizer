9e2c399c2173409855add24993b6241979d953a4
Aditya <adityakrsingh2604@gmail.com>
2025-07-22 23:12:04 +0530

    Revert "FIX: model sync in shared tour and doctor mode"
    This reverts commit 95a58751f0f4f3e3c4dc1d4392c76bca72fb4449.

--- activities/HumanBody.activity/js/activity.js
+++ activities/HumanBody.activity/js/activity.js
@@ -771,29 +771,7 @@ define([
 				showLeaderboard();
 			}
 
-			// ...existing code...
 			if (msg.action == "startDoctor") {
-				if (msg.content.modelName) {
-					updateBodyPartsForModel(msg.content.modelName);
-
-					// switch models, before starting doctor mode
-					if (msg.content.modelName !== currentModelName) {
-						switchModel(msg.content.modelName);
-
-						// Wait for model to load before starting doctor mode
-						setTimeout(() => {
-							showLeaderboard();
-							isPaintActive = false;
-							isLearnActive = false;
-							isTourActive = false;
-							isDoctorActive = true;
-						}, 500);
-						return;
-					}
-				} else {
-					updateBodyPartsForModel(currentModelName);
-				}
-
 				showLeaderboard();
 				isPaintActive = false;
 				isLearnActive = false;
@@ -1045,20 +1023,7 @@ define([
 			}
 
 			if (isDoctorActive) {
-				// Send doctor mode start message with model info - with safety check
-				if (presence && presence.getSharedInfo() && presence.getSharedInfo().id) {
-					presence.sendMessage(presence.getSharedInfo().id, {
-						user: presence.getUserInfo(),
-						action: "startDoctor",
-						content: {
-							players: players,
-							modelName: currentModelName,
-							bodyPartsData: bodyParts
-						},
-					});
-				}
-				ifDoctorHost = true;
-				startDoctorModePresence();
+				showLeaderboard();
 			}
 
 			if (isHost) {
@@ -1139,15 +1104,10 @@ define([
 				if (presence) {
 					showLeaderboard();
 
-					// Send doctor mode start message with current model info
 					presence.sendMessage(presence.getSharedInfo().id, {
 						user: presence.getUserInfo(),
 						action: "startDoctor",
-						content: {
-							players: players,
-							modelName: currentModelName,
-							bodyPartsData: bodyParts
-						},
+						content: players,
 					});
 					ifDoctorHost = true;
 					startDoctorModePresence();
